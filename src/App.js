fc2ec14ebf8350027de239c3623e210a2ff9ef38
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-23 12:21:26 +0200

    created tab component and tab highlight with mousemove events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Tab extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      highlightStyle: { left: 0, opacity: 0 },
+    };
+  }
+
+  moveHightlight = (e) => {
+    //update hightlight style to move highlight
+    this.setState({
+      highlightStyle: { left: e.nativeEvent.layerX - 150 },
+    });
+  };
+
+  removeHighlight = (e) => {
+    this.setState({
+      highlightStyle: { opacity: 0, left: e.nativeEvent.layerX - 150 },
+    });
+  };
+
+  render() {
+    return (
+      <div
+        className='browser__tab'
+        onMouseMove={this.moveHightlight}
+        onMouseOut={this.removeHighlight}
+      >
+        <div className='highlight' style={this.state.highlightStyle} />
+        <a>{this.props.link}</a>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -8,15 +43,9 @@ class App extends Component {
         <h1>Browser App</h1>
         <div className='browser'>
           <div className='browser__tabs'>
-            <div className='browser__tab'>
-              <a>Home</a>
-            </div>
-            <div className='browser__tab'>
-              <a>About</a>
-            </div>
-            <div className='browser__tab'>
-              <a>Features</a>
-            </div>
+            <Tab link='Home' />
+            <Tab link='About' />
+            <Tab link='Features' />
           </div>
 
           <div className='browser__viewport'>Pages Go Here</div>
